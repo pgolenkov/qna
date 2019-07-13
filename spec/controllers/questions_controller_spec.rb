a1b34605551e966e903b6cfac3c1860abfa34f3c
Pavel Golenkov <pgolenkov@gmail.com>
2019-07-13 18:31:38 +0500

    Create action for questions controller

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #new' do
+    it 'should render new view' do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid params' do
+      def post_question
+        post :create, params: { question: attributes_for(:question) }
+      end
+
+      it 'should create new question' do
+        expect { post_question }.to change { Question.count }.by(1)
+      end
+      it 'should redirect to created question' do
+        post_question
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid params' do
+      it 'should not create new question' do
+        expect { post :create, params: { question: attributes_for(:question, :invalid) } }.not_to change { Question.count }
+      end
+      it 'should render new view' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+end
