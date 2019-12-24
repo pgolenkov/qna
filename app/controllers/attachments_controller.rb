76f248aaa911a53dc62c894cfff32cc24e5140a9
Pavel Golenkov <pgolenkov@gmail.com>
2019-12-24 16:04:04 +0500

    Simplify attachments controller

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,18 +1,12 @@
 class AttachmentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :check_author
 
   def destroy
-    attachment.purge
-  end
-
-  private
-
-  def attachment
-    @attachment ||= ActiveStorage::Attachment.find(params[:id])
-  end
-
-  def check_author
-    head(:forbidden) unless current_user.author?(attachment.record)
+    @attachment = ActiveStorage::Attachment.find(params[:id])
+    if current_user.author?(@attachment.record)
+      @attachment.purge
+    else
+      head(:forbidden)
+    end
   end
 end
