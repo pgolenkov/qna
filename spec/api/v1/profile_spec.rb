59b428bcd2880e27ac209256d39687a99a05f6c2
Pavel Golenkov <pgolenkov@gmail.com>
2020-04-19 19:53:02 +0500

    Add users profile list api

--- spec/api/v1/profile_spec.rb
+++ spec/api/v1/profile_spec.rb
@@ -33,4 +33,45 @@ describe 'Profiles API', type: :request do
       end
     end
   end
+
+  describe 'GET /api/v1/profiles' do
+    let(:api_path) { '/api/v1/profiles' }
+    let(:me) { create :user }
+    let!(:users) { create_list :user, 2 }
+    let(:access_token) { create :access_token, resource_owner_id: me.id }
+
+    it_behaves_like 'API authorizable' do
+      let(:method) { :get }
+    end
+
+    context 'authorized' do
+      let(:users_json) { json['users'] }
+      let(:user_json) { json['users'].first }
+
+      before do
+        get api_path, params: { access_token: access_token.token }, headers: headers
+      end
+
+      it 'returns list of users' do
+        expect(users_json.size).to eq users.size
+      end
+
+      it 'returns all users without me' do
+        json_user_ids = users_json.map { |user_json| user_json['id'] }
+        expect(json_user_ids.sort).to eq(users.map(&:id).sort)
+      end
+
+      it 'returns public fields of user' do
+        %w[id email admin created_at updated_at].each do |attr|
+          expect(user_json[attr]).to eq users.first.send(attr).as_json
+        end
+      end
+
+      it 'doesn`t return private fields of user' do
+        %w[password encrypted_password].each do |attr|
+          expect(user_json).to_not have_key(attr)
+        end
+      end
+    end
+  end
 end
