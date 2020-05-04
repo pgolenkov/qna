bd8ef57e4dfd7f828294d4fbfe98d10fe00cd1ff
Pavel Golenkov <pgolenkov@gmail.com>
2020-05-04 10:51:58 +0500

    Notificate all subscribed users with new answer

--- app/services/notification.rb
+++ app/services/notification.rb
@@ -1,6 +1,7 @@
 class Services::Notification
   def new_answer(answer)
-    user = answer.question.user
-    NotificationMailer.new_answer(user, answer).deliver_later
+    answer.question.subscriptions.includes(:user).each do |subscription|
+      NotificationMailer.new_answer(subscription.user, answer).deliver_later
+    end
   end
 end
--- spec/services/notification_spec.rb
+++ spec/services/notification_spec.rb
@@ -5,12 +5,30 @@ RSpec.describe Services::Notification do
     let!(:user) { create :user }
     let(:question) { create :question }
     let(:question_owner) { question.user }
+
     let(:answer) { create :answer, question: question }
 
-    it 'sends new answer notification mail to owner of question only' do
-      expect(NotificationMailer).to receive(:new_answer).with(question_owner, answer).and_call_original
-      expect(NotificationMailer).not_to receive(:new_answer).with(user, answer)
-      subject.new_answer(answer)
+    context 'when there are no other subscribers except owner' do
+      it 'sends new answer notification mail to owner of question only' do
+        expect(NotificationMailer).to receive(:new_answer).with(question_owner, answer).and_call_original
+        expect(NotificationMailer).not_to receive(:new_answer).with(user, answer)
+        subject.new_answer(answer)
+      end
+    end
+
+    context 'when there are other subscribers' do
+      let(:subscribed_users) do
+        create_list(:user, 2).tap do |users|
+          users.each { |user| create :subscription, subscribable: question, user: user }
+        end
+      end
+
+      it 'sends new answer notification mail to all subsribed users of question' do
+        (subscribed_users + [question_owner]).each do |user|
+          expect(NotificationMailer).to receive(:new_answer).with(user, answer).and_call_original
+        end
+        subject.new_answer(answer)
+      end
     end
   end
 end
