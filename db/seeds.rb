8e331d4b4b2d6fe8dc3668b3858372c0b5077c77
Pavel Golenkov <pgolenkov@gmail.com>
2020-05-17 10:02:22 +0500

    Add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.new(email: 'admin@email.com', admin: true, password: '123456', password_confirmation: '123456')
+admin.skip_confirmation!
+admin.save!
+
+user = User.new(email: 'user@email.com', admin: true, password: '123456', password_confirmation: '123456')
+user.skip_confirmation!
+user.save!
+
+questions = Question.create!([
+  { title: 'Вопрос для примера 1', body: 'Сам текст вопроса для примера 1', user: admin },
+  { title: 'Вопрос для примера 2', body: 'Сам текст вопроса для примера 2', user: user }
+])
+
+answers = questions.map do |question|
+  question.answers.create!([
+    { body: 'Ответ 1', user: admin },
+    { body: 'Ответ 2', user: user }
+  ])
+end
