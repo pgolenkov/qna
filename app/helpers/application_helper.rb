4546c750bf2848a76d42f402d503650ab3e02d06
Pavel Golenkov <pgolenkov@gmail.com>
2020-06-17 12:26:57 +0500

    Move cache keys to helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def question_cache_key(question)
+    [signed_in?, can?(:create_vote, question), can?(:update, question), can?(:destroy, question), question]
+  end
+
+  def answer_cache_key(answer)
+    [
+      signed_in?,
+      can?(:make_best, answer),
+      can?(:create_vote, answer),
+      can?(:update, answer),
+      can?(:destroy, answer),
+      answer
+    ]
+  end
 end
